5e18b02474e85d2c0f9389fecd86bec6b647fbb3
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-10-09 17:07:19 +0700

    first commit

---
+++ src/pbo10118045latihan18kelipatan/PBO10118045Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan18kelipatan;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : Menghitung kelipatan
+ */
+public class PBO10118045Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i<=35;i+=3.5){
+            System.out.println(i);
+        }
+    }
+    
+}
